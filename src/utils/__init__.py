8b6018d6fefedcea2998bef9d0881f2e63407f33
superintendent2521 <67196752+superintendent2521@users.noreply.github.com>
2025-09-15 15:26:45 -0400

    some fixes for security issues and code cleanup

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -5,4 +5,4 @@ Contains utility functions and helpers.
 
 from .validation import is_valid_title, is_valid_branch_name, sanitize_filename
 
-__all__ = ['is_valid_title', 'is_valid_branch_name', 'sanitize_filename']
+__all__ = ['is_valid_title', 'is_valid_branch_name', 'sanitize_filename'] # IGNORE: R0801
